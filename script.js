20e695c818654214095dea961ad7a39829e2c953
Nikoloz Tskhondia <niksonn.cxondo@gmail.com>
2025-11-14 18:00:42 +0400

    completed index.html

---
+++ script.js
@@ -0,0 +1,38 @@
+let number = 10
+let sq = number * number
+
+if (sq > 100) {
+  console.log("მეტია ასზე")
+} else {
+  console.log("ნაკლებია ასზე")
+}
+
+
+let num = 60
+
+if (num > 50 && num < 100) {
+  console.log("შუაშია 50-100")
+} else {
+  console.log("არ არის მაგ დიაპაზონში")
+}
+
+let x = -5
+
+if (x == 0) {
+  console.log("ნულია")
+} else if (x < 0) {
+  console.log("უარყოფითია")
+} else {
+  console.log("დადებითია")
+}
+
+let sigrdze = 10
+let sigane = 8
+
+let p = 2 * (sigrdze + sigane)
+
+if (p > 50) {
+  console.log("პერიმეტრი დიდია 50-ზე")
+} else {
+  console.log("პერიმეტრი პატარაა ან ტოლია 50-ის")
+}
